f1fc8560bf6ef20d1e2110a2ff1a6a2f91553255
Akshat Gupta <akshat.g1707@gmail.com>
2024-08-24 11:29:38 +0530

    init

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	fmt.Println("hello")
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, Echo!"})
+	})
+	e.Start(":8080")
+}
