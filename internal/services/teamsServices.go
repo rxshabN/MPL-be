0584c928e6fd4142907213399b58e024fdebfa97
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-03 00:04:15 +0530

    fix: create team

--- internal/services/teamsServices.go
+++ internal/services/teamsServices.go
@@ -1,26 +1,23 @@
 package services
 
 import (
-	"encoding/json"
-
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"github.com/oik17/mpl-be/internal/database"
 	"github.com/oik17/mpl-be/internal/models"
 )
 
 func CreateTeam(team models.Teams) error {
 	db := database.DB.Db
-	membersJSON, err := json.Marshal(team.TeamMembers)
-	if err != nil {
-		return err
-	}
-	_, err = db.Exec(`INSERT INTO teams VALUES $1, $2, $3, $4, $5`, uuid.New(), team.TeamName, membersJSON, team.Score, team.HintNumber)
+	_, err := db.Exec(`
+        INSERT INTO team (team_id, team_name, team_members, score, hint_number)
+        VALUES ($1, $2, $3, $4, $5)`,
+		uuid.New(), team.TeamName, pq.Array(team.TeamMembers), team.Score, team.HintNumber)
 	if err != nil {
 		return err
 	}
 	return nil
 }
-
 func GetAllTeams() ([]models.Teams, error) {
 	db := database.DB.Db
 
