1b052c8a0c347b2058b2523e2b5cbefbe56580c3
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-03 22:33:36 +0530

    fix: restructure

---
+++ internal/services/leaderboardServices.go
@@ -0,0 +1,87 @@
+package services
+
+import (
+	"github.com/lib/pq"
+	"github.com/oik17/mpl-be/internal/database"
+	"github.com/oik17/mpl-be/internal/models"
+)
+
+func GetAllTeamsByScore() ([]models.Teams, error) {
+	db := database.DB.Db
+
+	rows, err := db.Query(`SELECT team_id, team_name, team_members, score, hint_number FROM team ORDER BY score`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var teams []models.Teams
+
+	for rows.Next() {
+		var team models.Teams
+		var members pq.StringArray
+
+		err := rows.Scan(&team.TeamID, &team.TeamName, &members, &team.Score, &team.HintNumber)
+		if err != nil {
+			return nil, err
+		}
+
+		team.TeamMembers = []string(members)
+		teams = append(teams, team)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return teams, nil
+}
+
+func UpdateTeamScore(teamID string, score int) error {
+	db := database.DB.Db
+	_, err := db.Exec(`UPDATE team SET score=$1 WHERE team_id=$2`, score, teamID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetAllTeamsByHint() ([]models.Teams, error) {
+	db := database.DB.Db
+
+	rows, err := db.Query(`SELECT team_id, team_name, team_members, score, hint_number FROM team ORDER BY hint_number`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var teams []models.Teams
+
+	for rows.Next() {
+		var team models.Teams
+		var members pq.StringArray
+
+		err := rows.Scan(&team.TeamID, &team.TeamName, &members, &team.Score, &team.HintNumber)
+		if err != nil {
+			return nil, err
+		}
+
+		team.TeamMembers = []string(members)
+		teams = append(teams, team)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return teams, nil
+}
+
+func UpdateTeamHint(teamID string, hint int) error {
+	db := database.DB.Db
+	_, err := db.Exec(`UPDATE team SET hint_number=$1 WHERE team_id=$2`, hint, teamID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
