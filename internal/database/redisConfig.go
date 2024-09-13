cee1a6c9e5648dfa7f7ad0d140a2b535733303c8
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-13 12:57:58 +0530

    fix: redis

--- internal/database/redisConfig.go
+++ internal/database/redisConfig.go
@@ -4,21 +4,28 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/oik17/mpl-be/internal/utils"
 	"github.com/redis/go-redis/v9"
 )
 
 func RedisConnect() {
+
+	redisDB, err := strconv.Atoi(utils.Config("REDIS_DB"))
+	if err != nil {
+		log.Fatalf("Invalid REDIS_DB value: %v", err)
+	}
+
 	client := redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%s", utils.Config("REDIS_HOST"), utils.Config("REDIS_PORT")),
 		Password: utils.Config("REDIS_PASSWORD"),
-		DB:       0,
+		DB:       redisDB,
 	})
 
 	ctx := context.Background()
 
-	err := client.Set(ctx, "foo", "bar", 0).Err()
+	err = client.Set(ctx, "foo", "bar", 0).Err()
 	if err != nil {
 		panic(err)
 	}
