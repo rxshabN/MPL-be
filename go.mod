3cef8b026dcb49ae26b5d780c8d4737b28a840b7
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-20 11:46:45 +0530

    fix: auth

--- go.mod
+++ go.mod
@@ -5,27 +5,26 @@ go 1.22
 toolchain go1.23.1
 
 require (
+	github.com/AfterShip/email-verifier v1.4.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo-jwt/v4 v4.2.0
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.6.1
 	golang.org/x/crypto v0.27.0
 )
 
 require (
-	github.com/AfterShip/email-verifier v1.4.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hbollon/go-edlib v1.6.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/redis/go-redis v6.15.9+incompatible // indirect
-	github.com/redis/go-redis/v9 v9.6.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/net v0.29.0 // indirect
--- internal/routes/teamRoutes.go
+++ internal/routes/teamRoutes.go
@@ -1,24 +1,26 @@
 package routes
 
 import (
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/oik17/mpl-be/internal/controllers"
+	"github.com/oik17/mpl-be/internal/middleware"
 )
 
 func TeamRoutes(e *echo.Echo) {
 	r := e.Group("/teams")
 
-	r.POST("/createTeam", controllers.CreateTeam)
+	r.POST("/createTeam", controllers.CreateTeam, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
 	r.GET("/getAllTeams", controllers.GetAllTeams)
 
 	r.GET("/getTeamsByScore", controllers.GetAllTeamsByScore)
-	r.PUT("/updateScore/:teamID", controllers.UpdateTeamScore)
+	r.PUT("/updateScore/:teamID", controllers.UpdateTeamScore, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
 
 	r.GET("/getTeamsByHint", controllers.GetAllTeamsByHints)
-	r.PUT("/updateHint/:teamID", controllers.UpdateTeamHint)
+	r.PUT("/updateHint/:teamID", controllers.UpdateTeamHint, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
 
-	r.GET("/getTeamHint/:teamID", controllers.GetTeamHint)
+	r.GET("/getTeamHint/:teamID", controllers.GetTeamHint, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
 
-	r.GET("/startTimer", controllers.StartTimer)
+	r.GET("/startTimer", controllers.StartTimer, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
 	r.GET("/getTimer", controllers.GetTimeLeft)
 }
