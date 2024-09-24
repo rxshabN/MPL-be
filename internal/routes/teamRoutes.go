67290756bdf33edff666c6ace0596196d4258b0f
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-24 14:28:18 +0530

    fix

--- internal/routes/teamRoutes.go
+++ internal/routes/teamRoutes.go
@@ -1,26 +1,24 @@
 package routes
 
 import (
-	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/oik17/mpl-be/internal/controllers"
-	"github.com/oik17/mpl-be/internal/middleware"
 )
 
 func TeamRoutes(e *echo.Echo) {
 	r := e.Group("/teams")
 
-	r.POST("/createTeam", controllers.CreateTeam, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
+	r.POST("/createTeam", controllers.CreateTeam)
 	r.GET("/getAllTeams", controllers.GetAllTeams)
 
 	r.GET("/getTeamsByScore", controllers.GetAllTeamsByScore)
-	r.PUT("/updateScore/:teamID", controllers.UpdateTeamScore, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
+	r.PUT("/updateScore/:teamID", controllers.UpdateTeamScore)
 
 	r.GET("/getTeamsByHint", controllers.GetAllTeamsByHints)
-	r.PUT("/updateHint/:teamID", controllers.UpdateTeamHint, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
+	r.PUT("/updateHint/:teamID", controllers.UpdateTeamHint)
 
-	r.GET("/getTeamHint/:teamID", controllers.GetTeamHint, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
+	r.GET("/getTeamHint/:teamID", controllers.GetTeamHint)
 
-	r.GET("/startTimer", controllers.StartTimer, echojwt.JWT(controllers.JWTSecret), middleware.Protected)
+	r.GET("/startTimer", controllers.StartTimer)
 	r.GET("/getTimer", controllers.GetTimeLeft)
 }
